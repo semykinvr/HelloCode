ff8d387ac35b174fbd87917194cd1dee89b563a2
semykinvr <semykinvr@mail.ru>
2022-01-30 20:54:46 +0300

    Пример 004 Деление двух чисел

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,7 @@
+﻿// УМНОЖЕНИЕ ДВУХ ЧИСЕЛ
+Console.WriteLine("Hello, World!"); 
+double numberA = 12; 
+double numberB = 5; 
+// int result = numberA + numberB;  
+Console.Write("Деление двух чисел = "); 
+Console.WriteLine(numberA / numberB); 
