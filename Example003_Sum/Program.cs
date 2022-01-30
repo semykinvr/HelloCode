e160cb0c831a0ba50bac5b60017f930558f3fdf8
semykinvr <semykinvr@mail.ru>
2022-01-30 20:35:47 +0300

    Пример 003 Сумма двух чисел

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// СЛОЖЕНИЕ ДВУХ ЧИСЕЛ
+Console.WriteLine("Hello, World!"); 
+int numberA = 31; 
+int numberB = 5; 
+int result = numberA + numberB;  
+Console.Write("Сумма двух чисел = "); 
+Console.WriteLine(result); 
